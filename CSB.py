460af5ad976407e5a10df63ac57e111ea48971b4
S V Sree Harsha <svsreeharsha@gmail.com>
2026-03-12 10:31:44 +0530

    Update CSB.py

--- CSB.py
+++ CSB.py
@@ -1,20 +1,20 @@
 import pandas as pd
 df = pd.read_csv('customer_shopping_behavior.csv')
-#print(df.head())
-#print(df.info())
-#print(df.describe())
-#print(df.isnull().sum())
+print(df.head())
+print(df.info())
+print(df.describe())
+print(df.isnull().sum())
 df['Review Rating'] = df.groupby('Category')['Review Rating'].transform(lambda x: x.fillna(x.median()))
-#print(df.isnull().sum())
+print(df.isnull().sum())
 df.columns = df.columns.str.lower()
 df.columns = df.columns.str.replace(' ', '_')
-#print(df.columns)
+print(df.columns)
 df = df.rename(columns={'purchase_amount_(usd)': 'purchase_amount'})
-#print(df.columns)
+print(df.columns)
       #Create a column
 labels = ['Young Adult', 'Adult', 'Middle-aged', 'Senior']
 df['age_group'] = pd.qcut(df['age'], q=4,labels = labels)
-#print(df[['age', 'age_group']].head(10))
+print(df[['age', 'age_group']].head(10))
 # Purchase_frequency_days column creation
 
 frequency_mapping ={
@@ -33,3 +33,4 @@ print(df[['discount_applied', 'promo_code_used']].head(10))
 print((df['discount_applied'] == df['promo_code_used']).all())
 df = df.drop('promo_code_used', axis=1)
 print(df.columns)
+
